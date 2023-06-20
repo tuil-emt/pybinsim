ef1b498ff1a69b0e315ae7e36ce9213c8043cfe9
Erik Hübner <erik.huebner@tu-ilmenau.de>
2023-06-20 16:45:51 +0200

    Disable block interpolation, which seems to have done some damage. Add more output to failure messages.
    Former-commit-id: cfdccd3c9785639deeacb35cd67832454c8d6069

--- tests/test_convolution.py
+++ tests/test_convolution.py
@@ -37,14 +37,14 @@ def test_convolution_basic():
     test_hp_filter = np.zeros((HPFILTERSIZE, 2))
     test_hp_filter[0,0] = 1
     test_hp_filter[0,1] = 1
-    soundfilename = "../example/signals/speech2_48000_mono.wav"
+    soundfilename = "example/signals/speech2_48000_mono.wav"
 
     ## First: Pybinsim convolution
     # Create input buffer, convolver
     input_BufferHP = InputBufferMulti(BLOCKSIZE, 2, 'cpu')
     input_Buffer = InputBufferMulti(BLOCKSIZE, 1, 'cpu')
 
-    convolver = ConvolverTorch(FILTERSIZE, BLOCKSIZE, False, 1, True, 'cpu')
+    convolver = ConvolverTorch(FILTERSIZE, BLOCKSIZE, False, 1, False, 'cpu')
     convolver.active = 'True'
     convolverHP = ConvolverTorch(HPFILTERSIZE, BLOCKSIZE, True, 2, False, 'cpu')
     convolverHP.active = 'True'
@@ -116,14 +116,21 @@ def test_convolution_basic():
     result_matrix_pybinsim[0, :] = np.divide(result_matrix_pybinsim[0, :],np.max(np.abs(result_matrix_pybinsim[0, :])))
     result_matrix_pybinsim[1, :] = np.divide(result_matrix_pybinsim[1, :], np.max(np.abs(result_matrix_pybinsim[1, :])))
 
-    left_correct = np.allclose(result_matrix_pybinsim[0, :], left_hp_part)
-    right_correct = np.allclose(result_matrix_pybinsim[1, :], right_hp_part)
+    left_correct = np.isclose(result_matrix_pybinsim[0, :], left_hp_part)
+    right_correct = np.isclose(result_matrix_pybinsim[1, :], right_hp_part)
 
+    ## These were the values that still failed
+    # print(result_matrix_pybinsim[0, 1455])
+    # print(left_hp_part[1455])
+    # print(result_matrix_pybinsim[0, 1574])
+    # print(left_hp_part[1574])
+    # print(result_matrix_pybinsim[0, 1876])
+    # print(left_hp_part[1876])
 
-    assert left_hp_part == approx(result_matrix_pybinsim[0, :], abs=ACCURACY*100)
-    assert right_hp_part == approx(result_matrix_pybinsim[1, :], abs=ACCURACY*100)
-    #assert left_correct == True, f"Left channel convolution faulty"
-    #assert right_correct == True, f"Right channel convolution faulty"
+    assert left_hp_part == approx(result_matrix_pybinsim[0, :], abs=PCM16_ACCURACY*1)
+    assert right_hp_part == approx(result_matrix_pybinsim[1, :], abs=PCM16_ACCURACY*1)
+    assert left_correct.all() == True, f"Left channel convolution faulty at {[i for i in range(0, FILTERSIZE) if left_correct[i] == False]}"
+    assert right_correct.all() == True, f"Right channel convolution faulty at {[i for i in range(0, FILTERSIZE) if right_correct[i] == False]}"
 
     print('Done')
 
